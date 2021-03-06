d0a5e6d871f2ce9974d8f6e05435489b3024cfc1
truongphhung <truongphuhung9@gmail.com>
2021-03-07 00:21:39 +0700

    Seperate into function

--- _10species.py
+++ _10species.py
@@ -1,6 +1,34 @@
 import csv
 import sys
 
+def tenSpecies(species):
+    """
+    argument:
+    -dict species_list: list of species, read from csv wwith DictReader
+    """
+
+    process = []
+    group = []
+    index = 1
+
+    for specie in species:
+        # Checking if the group already has 10 species
+        if len(group) == 10:
+            group.clear()
+            index += 1
+
+        # Add the group to Specie
+        specie['Group'] = index
+
+        # Add specie to the group if it doesnot exist
+        if specie['Specie'] not in group:
+            group.append(specie['Specie'])
+
+        # Add this to new block
+        process.append(specie)
+
+    return process
+
 
 def main():
     """
@@ -16,37 +44,18 @@ def main():
     """
     Rule: choose top 10 species appear first and group them.
     """
-    species_list = []
 
     # read file csv format
     with open(sys.argv[1], mode='r') as csv_file:
         data = csv.DictReader(csv_file)
         
-        process = []
-        group = []
-        index = 1
-
-        for specie in data:
-            # Checking if the group already has 10 species
-            if len(group) == 10:
-                group.clear()
-                index += 1
-
-            # Add the group to Specie
-            specie['Group'] = index
-
-            # Add specie to the group if it doesnot exist
-            if specie['Specie'] not in group:
-                group.append(specie['Specie'])
-
-            # Add this to new block
-            process.append(specie)
+        result = tenSpecies(data)        
 
         with open('tenSpecies.csv', mode='w') as ofile:
             fields = ['Specie', 'Individual', 'Group']
             writer = csv.DictWriter(ofile, fieldnames=fields)
             writer.writeheader()
-            writer.writerows(process)
+            writer.writerows(result)
 
 if __name__ == "__main__":
     main()
